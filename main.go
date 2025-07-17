48b57779d28c835182d6bbbdf83c077e973b70ee
zhaopengfei <20298039+justseemore@users.noreply.github.com>
2025-07-17 14:11:40 +0800

    init

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/justseemore/sshm/cmd"
 )
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		os.Exit(1)
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) && exitErr.ExitCode() == 130 {
+			// 静默退出或自定义处理
+			os.Exit(0)
+		}
+		fmt.Println(err.Error())
 	}
 }
